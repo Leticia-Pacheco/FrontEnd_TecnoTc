fe525f80374d17fb003425db67024cdd1442a42d
DANIEL VITOR DE SOUZA <20185440@CFP127ALUNO.LOCAL>
2021-05-05 15:22:29 -0300

    router

--- src/router.js
+++ src/router.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-
-import Login from './pages/Login';
-import Register from './pages/Register';
-import { isSignedIn } from './service/security';
-// import ConfirmEmail from './pages/ConfirmEmail';
-import OrderConfirmEmail from './pages/OrderConfirmEmail';
-import ConfirmEmail from './pages/ConfirmEmail';
-function PrivateRoute({ children, ...rest }) {
-  if (isSignedIn()) {
-=======
 import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -23,7 +10,6 @@ import RedefinirSenha from './components/ModalRedefinirSenha';
 
 function PrivateRoute({children, ...rest}) {
   if(isSignedIn()) {
->>>>>>> 527f9d738fe29e5a8d964fb6fc2d1122860626eb
     return <Route {...rest}>{children}</Route>;
   } else {
     return <Redirect to="/" />;
@@ -43,13 +29,6 @@ function Router() {
         <PrivateRoute path="/home">
           <Home />
         </PrivateRoute>
-<<<<<<< HEAD
-        <Route path="/orderconfirmemail">
-          <OrderConfirmEmail />
-        </Route>
-        <Route path="/confirmemail">
-          <ConfirmEmail/>
-=======
         <Route path="/confirmEmail">
           <OrderConfirmEmail />
         </Route>
@@ -58,7 +37,6 @@ function Router() {
         </Route>
         <Route path="/redefinirsenha">
           <RedefinirSenha />
->>>>>>> 527f9d738fe29e5a8d964fb6fc2d1122860626eb
         </Route>
       </Switch>
     </BrowserRouter>
