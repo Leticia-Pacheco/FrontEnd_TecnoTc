abaa3ffb5300012a21b55dc4712b613f1bf8f73c
biancagothic892 <biancagothicg@gmail.com>
2021-05-26 19:04:16 -0300

    consumindo api  de crianção de anotações e melhoria de codigo

--- src/components/ModalCriarAnotacoes/index.js
+++ src/components/ModalCriarAnotacoes/index.js
@@ -9,6 +9,7 @@ function CreateAnotacoes({handleClose}) {
         title: "",
         text: "",
     });
+    const handlFunctionFecharModal = () => handleClose;
     const handleInput = (e) => {
         setAnnotation({...newAnnotation, [e.target.id]: e.target.value});
     };
@@ -24,21 +25,29 @@ function CreateAnotacoes({handleClose}) {
             });
 
             signIn(response.data);
+            handlFunctionFecharModal();
         } catch(error) {
             console.error(error);
             alert(error.response.data.error);
         }
+
     };
     return (
         <>
             <Overlay>
-                <Container onSubmit={handleAddAnnotation}>
+                <Container onSubmit=
+                    {
+                        handleAddAnnotation
+                        &&
+                        handlFunctionFecharModal
+                    }>
                     <span onClick={handleClose}>X</span>
-
                     <h2>Criar uma anotação</h2>
 
-                    <p>As anotações servem para que você se oriente durante o processo de construção de seu projeto.
-                    Elas são úteis para que não se esqueça das tarefas mais importantes! Fique atento!
+                    <p>As anotações servem para que você se oriente durante o
+                    processo de construção de seu projeto.
+                    Elas são úteis para que não se esqueça das tarefas mais
+                    importantes! Fique atento!
                     </p>
 
                     <h3>Título da anotação:</h3>
@@ -47,6 +56,7 @@ function CreateAnotacoes({handleClose}) {
                         placeholder="Digite o título da anotação aqui"
                         value={newAnnotation.title}
                         handler={handleInput}
+                        required
                     />
 
                     <h3>Descrição:</h3>
@@ -54,8 +64,8 @@ function CreateAnotacoes({handleClose}) {
                         id="text"
                         handler={handleInput}
                         value={newAnnotation.text}
-                        rows="7"
-                        placeholder="Digite a descrição da anotação aqui"></Input>
+                        placeholder="Digite a descrição da anotação aqui"
+                        required></Input>
                     <button>
                         Criar anotação
                     </button>
