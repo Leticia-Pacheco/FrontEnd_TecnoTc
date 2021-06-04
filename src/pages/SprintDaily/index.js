0f4b18f90b3712d697a8afc319b7f2b7255521a5
O Daniel <soumenorgame@gmail.com>
2021-06-04 16:37:53 -0300

    Criação de reunião diaria e sprint retrospective prontas

--- src/pages/SprintDaily/index.js
+++ src/pages/SprintDaily/index.js
@@ -1,7 +1,60 @@
+import { useState } from "react";
+import { useHistory, useParams } from "react-router";
 import { Container, Conteudo, Form } from "./styles";
 
-function SprintRetrospective () {
-    return(
+function SprintDaily() {
+
+    const [sprintDaily, setSprintDaily] = useState({
+        doneYesterday: '',
+        goingToDoDay: '',
+        someObstacle: '',
+    });
+    const [message, setMessage] = useState(undefined)
+
+    const [sprint, setSprint] = useState([]);
+
+    const { sprintId } = useParams();
+
+    const history = useHistory();
+
+    const findSprint = async () => {
+
+        const response = await api.get(`/sprints/${sprintId}`);
+        console.log(response.data)
+
+        if (!response.data)
+            return history.push("/profile")
+        setSprint(response.data.Group)
+
+        console.log(response.data.Group)
+    }
+
+    useEffect(() => {
+        findSprint();
+    }, [])
+
+    const handleInput = (e) => {
+        setSprintDaily({ ...sprintDaily, [e.target.id]: e.target.value });
+    }
+
+    const createSprintDaily = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post(`/dailyScrum/${sprintId}`, {
+                doneYesterday: sprintDaily.doneYesterday,
+                goingToDoDay: sprintDaily.goingToDoDay,
+                someObstacle: sprintDaily.someObstacle
+            });
+
+            setMessage({ title: "Tudo certo", description: "Reunião diaria criada" });
+        } catch (error) {
+            console.error(error);
+            setMessage({ title: "algo deu errado", description: error.response.data.error });
+        }
+    }
+
+    return (
         <Container>
             <Form>
                 <Conteudo>
@@ -10,22 +63,22 @@ function SprintRetrospective () {
                     <p>Preencha corretamente todos os campos.</p>
 
                     <span>Nome do projeto</span>
-                    <input type="text"/>
+                    <input type="text" />
 
                     <span>O que você fez ontem?</span>
-                    <textarea  placeholder="Escreva aqui o que foi feito ontem por você"/>
+                    <textarea id="doneYesterday" placeholder="Escreva aqui o que foi feito ontem por você" onChange={handleInput} />
 
                     <span>O que fará hoje?</span>
-                    <textarea placeholder="Escreva aqui o que você fará hoje"/>
+                    <textarea id="goingToDoDay" placeholder="Escreva aqui o que você fará hoje" onChange={handleInput} />
 
                     <span>Houve algum conflito em seu grupo?</span>
-                    <textarea placeholder="Escreva aqui se houve algum conflito em seu grupo"/>
+                    <textarea id="someObstacle" placeholder="Escreva aqui se houve algum conflito em seu grupo" onChange={handleInput}/>
 
-                    <button>Enviar sua Sprint Diária!</button>
+                    <button onClick={createSprintDaily}>Enviar sua Sprint Diária!</button>
                 </Conteudo>
             </Form>
         </Container>
     );
 }
 
-export default SprintRetrospective
+export default SprintDaily
