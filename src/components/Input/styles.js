922210619f7e71adbd4f47779fd11b9e8d69a68e
Letícia Pacheco <le.pacheco0310@hotmail.com>
2021-06-24 14:54:12 -0300

    Styles

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div`  
   > input {
-    width: 450px;
+    width: 100%;
     height: 36px;
 
     font-size: 16px;
