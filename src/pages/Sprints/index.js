dfe4911f478b15cbba345c2701f9c3dbf5b65619
Karina Soares <lkarinas.silva@gmail.com>
2021-07-06 02:26:17 -0300

    tela de sprints

--- src/pages/Sprints/index.js
+++ src/pages/Sprints/index.js
@@ -1,9 +1,29 @@
-import { Container, Content } from "./styles";
+import { Container, Content, Titulo, LogoTecnoTc, SelectSprints, ContainerSprints, SprintBacklog, SprintRevew, SprintDaily } from "./styles";
+import ImageLogo from '../../assets/logos/logo_fundo_roxo_png.png';
 
 function Sprints() {
     return(
         <Container>
             <Content>
+                <Titulo> 
+                    <LogoTecnoTc>
+                        <img src={ImageLogo} alt="Logo" title="Logo" />
+                    </LogoTecnoTc>
+                    <h1>Sprints</h1>
+                </Titulo>
+                <SelectSprints>
+                <select id="categories" label="categorias">
+                    <option value="">Selecione</option>
+                    <option>1º Sprint</option>
+                    <option>2º Sprint</option>
+                    <option>3º Sprint</option>
+                </select>
+                </SelectSprints>
+                <ContainerSprints>
+                    <SprintBacklog> Sprint Backlog </SprintBacklog>
+                    <SprintRevew> Sprint Revew </SprintRevew>
+                    <SprintDaily> Sprint Daily </SprintDaily>
+                </ContainerSprints>
                 
             </Content>
         </Container>
